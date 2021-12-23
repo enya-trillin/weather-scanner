f202af4db8fdb95cf32af7dae7cf3a49f0311c29
enya-trillin <96012908+enya-trillin@users.noreply.github.com>
2021-12-24 03:14:47 +0800

    Linked weather icons menu from API for real time weather

--- src/app.js
+++ src/app.js
@@ -16,21 +16,30 @@ function formatDate(timestamp){
 }
 
 function displayTemperature(response){
+    console.log(response);
     let temperatureElement=document.querySelector("#temperature");
     let cityElement=document.querySelector("#city");
     let descriptionElment=document.querySelector("#description");
     let humidityElment=document.querySelector("#humidity");
     let windElement=document.querySelector("#wind");
     let dateElement=document.querySelector("#date");
+    let iconElement=document.querySelector("#icon");
     temperatureElement.innerHTML=Math.round(response.data.main.temp);
     cityElement.innerHTML=response.data.name;
     descriptionElment.innerHTML=response.data.weather[0].description;
     humidityElment.innerHTML=response.data.main.humidity;
     windElement.innerHTML=Math.round(response.data.wind.speed);
     dateElement.innerHTML=formatDate(response.data.dt*1000);
-}
+    iconElement.setAttribute(
+        "src", 
+        `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute(
+        "alt",
+        response.data.weather[0].description);
+    }
 
-let apiKey="619ae46dd09153325c06122c8c0b4325";      
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=Bali&appid=${apiKey}&units=metric`;
+let apiKey="619ae46dd09153325c06122c8c0b4325";     
+let city="Bali"; 
+let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
