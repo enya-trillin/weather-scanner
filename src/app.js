ffdcb56a693f647d5cc929220c938297b29b830f
enya-trillin <96012908+enya-trillin@users.noreply.github.com>
2021-12-24 16:15:41 +0800

    Changed default city to Singapore

--- src/app.js
+++ src/app.js
@@ -82,4 +82,4 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink=document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-search("Bali");
+search("Singapore");
