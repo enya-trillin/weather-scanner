bd49f4e63af12e8aacc477d9dd9d27c2aa5f6bb5
enya-trillin <96012908+enya-trillin@users.noreply.github.com>
2021-12-24 14:52:11 +0800

    Added search engine feature

--- src/app.js
+++ src/app.js
@@ -38,8 +38,20 @@ function displayTemperature(response){
         response.data.weather[0].description);
     }
 
-let apiKey="619ae46dd09153325c06122c8c0b4325";     
-let city="Bali"; 
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city){
+    let apiKey="619ae46dd09153325c06122c8c0b4325";     
+    let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
 
-axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    let cityInputElement=document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+search("New York");
+
+let form=document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
