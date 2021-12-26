5373bce5de71b1433081cbdb86d738e138f00ed7
enya-trillin <96012908+enya-trillin@users.noreply.github.com>
2021-12-26 13:41:04 +0800

    Overwrote Forecast HTML using JS

--- src/app.js
+++ src/app.js
@@ -12,6 +12,35 @@ function formatDate(timestamp){
     let day=days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
 
+}
+
+function displayForecast(){
+    let forecastElement=document.querySelector("#forecast");
+    let days=["Sun","Mon","Tue","Wed"];
+    
+    let forecastHTML=`<div class="row">`;
+  
+    days.forEach(function(day){    
+    forecastHTML= forecastHTML +
+    `
+    <div class="col-2">
+      <div class="weather-forecast-date">${day}</div>
+      
+      <img
+      src="http://openweathermap.org/img/wn/04d@2x.png"
+      alt=""
+      width="40"
+      />
+      
+      <div class="weather-forecast-temperatures">
+        <span class="weather-forecast-temperature-max">31°</span> 
+        <span class="weather-forecast-temperature-min">27°</span>
+      </div>
+    </div>
+  `;
+  });
+  forecastHTML=forecastHTML+`</div>`;
+  forecastElement.innerHTML=forecastHTML;
 
 }
 
@@ -24,6 +53,7 @@ function displayTemperature(response){
     let windElement=document.querySelector("#wind");
     let dateElement=document.querySelector("#date");
     let iconElement=document.querySelector("#icon");
+
 
     celsiusTemperature =response.data.main.temp;
 
@@ -83,3 +113,4 @@ let celsiusLink=document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Singapore");
+displayForecast();
